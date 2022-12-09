3f8247260ea1b51c905998aebd08071a974de794
Himank Suiwala <himanksuiwala@gmail.com>
2022-12-09 11:03:43 +0530

    Changes in CORS url

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const allowed = [
 const app = express();
 app.use(
   cors({
-    origin: "https://tomato-himanksuiwala-gmailcom.vercel.app/",
+    origin: "https://tomato-himanksuiwala-gmailcom.vercel.app",
     credentials: true,
     allowedHeaders: ['Content-Type'],
     methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
