ba714a58afda233b5330deb1baa1e07f4b359060
Himank Suiwala <himanksuiwala@gmail.com>
2022-12-09 10:38:34 +0530

    Changes in CORS url

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const allowed = [
 const app = express();
 app.use(
   cors({
-    origin: "https://tomato-lac.vercel.app",
+    origin: "asxd.com",
     credentials: true,
     methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
   })
